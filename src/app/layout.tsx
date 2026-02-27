8cac03175e0938dd03b25929128a7d3315d27ba6
Mamie Cotton <admin@gigworldtoday.com>
2026-02-26 22:43:08 -0800

    chore(layout): add Google Analytics gtag scripts to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1DMN4WWE51" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);} 
+              gtag('js', new Date());
+              gtag('config', 'G-1DMN4WWE51');
+            `,
+          }}
+        />
+      </head>
       <body>
         <div className="fixed inset-0 -z-10">
           <img
