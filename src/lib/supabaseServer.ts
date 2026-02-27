f47aa572b1a6cd7d4c884bdbf5973adf47fdae24
Mamie Cotton <admin@gigworldtoday.com>
2026-02-26 23:16:56 -0800

    chore(supabase): prefer service role key for server client

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 export const createServerSupabase = () => {
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       auth: {
         persistSession: false,
