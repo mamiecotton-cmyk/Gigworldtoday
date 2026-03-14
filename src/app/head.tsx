0a3df38c8d46ac1c5a2001b439eb605eca39a044
Mamie Cotton <admin@gigworldtoday.com>
2026-03-13 23:24:47 -0700

    Add static analytics script tag to head for verifier detection

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,12 +2,7 @@ export default function Head() {
   return (
     <>
       <meta name="fo-verify" content="1b6c9375-9cc9-4b2d-90a2-f950d0538677" />
-      <script data-noptimize="1" data-cfasync="false" data-wpfc-render="false" dangerouslySetInnerHTML={{ __html: `(function () {
-          var script = document.createElement("script");
-          script.async = 1;
-          script.src = 'https://emrldtp.com/NTA3Mjg5.js?t=507289';
-          document.head.appendChild(script);
-      })();` }} />
+        <script data-noptimize="1" data-cfasync="false" data-wpfc-render="false" async src="https://emrldtp.com/NTA3Mjg5.js?t=507289"></script>
     </>
   );
 }
